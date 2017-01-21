33645a99e86626745071444ef654db1e00456ae8
Alexander Wåland <alexander@width.se>
2017-01-21 17:30:45 +0100

    Fix

--- src/client/app/shelters/shelters.component.ts
+++ src/client/app/shelters/shelters.component.ts
@@ -1,8 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {ApiService, Shelter} from '../shared/api/api.service';
-import {Observable} from "rxjs/Observable";
+import {Component} from '@angular/core';
 import {SheltersUserStateService} from "./user-state/shelters.user-state.service";
-import {ActivatedRoute} from "@angular/router";
 
 /**
  * This class represents the lazy loaded HomeComponent.
@@ -12,6 +9,7 @@ import {ActivatedRoute} from "@angular/router";
   selector: 'sd-app',
   templateUrl: 'shelters.component.html',
   styleUrls: ['shelters.component.css'],
+
 })
 
 export class SheltersComponent {
