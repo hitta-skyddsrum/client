ac0bb9f04074733842c24f614453aa6a22cbcafa
Alexander Wåland <alexander@width.se>
2017-02-18 23:35:44 +0100

    UI bugfix

--- src/app/shelters/shelters.component.ts
+++ src/app/shelters/shelters.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, NgZone } from '@angular/core';
 import { SheltersUserStateService } from './user-state/shelters.user-state.service';
 
 /**
@@ -17,7 +17,8 @@ export class SheltersComponent {
   public showBouncer: boolean = false;
 
   constructor(
-    protected sheltersUserStateService: SheltersUserStateService
+    protected sheltersUserStateService: SheltersUserStateService,
+    private zone: NgZone
   ) {
 
     sheltersUserStateService.currentStep$.subscribe(
@@ -26,8 +27,10 @@ export class SheltersComponent {
   }
 
   private setCurrentStep(step: number) {
-    this.isOpen = true;
-    this.currentStep = step;
-    this.progressBarWidth = (step - 1) * (100 / 3);
+    this.zone.run(() => {
+      this.isOpen = true;
+      this.currentStep = step;
+      this.progressBarWidth = (step - 1) * (100 / 3);
+    });
   }
 }
